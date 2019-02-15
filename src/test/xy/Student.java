b39c8880f568bc0013650cbb00cfcbdc3665a393
hexy <xiuyang91318@163.com>
2019-02-15 16:59:38 +0800

    测试

---
+++ src/test/xy/Student.java
@@ -0,0 +1,66 @@
+package test.xy;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Student implements Serializable {
+
+    private static final long serialVersionUID = 3210543561763669773L;
+    private String name;
+    private Integer age;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Student() {
+    }
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) &&
+                Objects.equals(age, student.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/test/xy/Test.java
+++ src/test/xy/Test.java
@@ -1,8 +1,15 @@
 package test.xy;
 
+import java.util.Random;
+
 public class Test {
     //测试
     public static void main(String[] args) {
         System.out.println("测试");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        Random random = new Random();
+
     }
 }
